9458b624124c3420f3ac180c74f91614d1cb7e77
atif <ratul0947@gmail.com>
2024-05-30 16:45:15 +0600

    feat: download concurrency

--- src/download.js
+++ src/download.js
@@ -4,7 +4,7 @@ module.exports = {
 	downloadSingleVideo: async ({ m3u8Url, filename, savedir = "media" }) => {
 		await download({
 			quality: "best",
-			concurrency: 40,
+			concurrency: 25,
 			outputFile: `${savedir}/${filename}.mp4`,
 			streamUrl: m3u8Url,
 		});
